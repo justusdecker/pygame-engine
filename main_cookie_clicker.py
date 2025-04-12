28f3d216ac8cbf5e3318b6ea114295cc3afd83b0
justusdecker <justus.d2025@gmail.com>
2025-04-12 04:15:16 +0200

    0.0.20

--- main_cookie_clicker.py
+++ main_cookie_clicker.py
@@ -24,9 +24,59 @@ def test_print(*args):
 class CookieClicker:
     def __init__(self):
         self.cookies = 0
+        
+        self.upgrades = {
+            'cursor': 0,
+            'grandma': 0,
+            'farm': 0,
+            'mine': 0,
+            'factory': 0
+        }
+        #efficiency upgrades / doubles the default output
+        self.u_cursor = 1
+        self.u_grandma = 1
+        self.u_farm = 1
+        self.u_mine = 1
+        self.u_factory = 1
+        
         self.lvl_click = 1
+    def get_auto_clicker(self):
+        CURSOR = self.upgrades['cursor'] * 0.1 * self.u_cursor
+        GRANDMA = self.upgrades['grandma'] * self.u_grandma
+        FARM = self.upgrades['farm'] * 8 * self.u_farm
+        MINE = self.upgrades['mine'] * 8 * self.u_mine
+        FACTORY = self.upgrades['factory'] * 47 * self.u_factory
+        
+        return CURSOR + GRANDMA + FARM + MINE + FACTORY
+    def update(self):
+        self.cookies += GLOBAL_DELTA_TIME.get() * self.get_auto_clicker()
+    def get_price(self,key):
+        return int({'cursor': 15 + (15 * self.upgrades['cursor'] * 0.2),
+         'grandma': 100 + (100*self.upgrades['grandma']*0.2),
+         'farm': 1_100 + (1_100*self.upgrades['farm']*0.2),
+         'mine': 12_000 + (12_000*self.upgrades['mine']*0.2),
+         'factory': 130_000 + (130_000*self.upgrades['factory']*0.2)
+         }[key])
+    def upgrade(self,lvl:str):
+        price = self.get_price('cursor')
+        if self.get() >= price:
+            self.cookies -= price
+            self.upgrades[lvl] += 1
+    def buy_cursor(self,*_):
+        self.upgrade('cursor')
+    def buy_grandma(self,*_):
+        self.upgrade('grandma')
+            
+    def buy_farm(self,*_):
+        self.upgrade('farm')
+    def buy_mine(self,*_):
+        self.upgrade('mine')
+    def buy_factory(self,*_):
+        self.upgrade('factory')
     def click(self):
-        self.cookies += self.lvl_click
+        self.cookies += 1
+    def get(self):
+        return int(self.cookies)
 
 class App(Application):
     def __init__(self):
@@ -45,11 +95,11 @@ class App(Application):
                 },
             on_press_callback = self.on_cookie_click
         )
-        self.lvl_click_upgrade_button = UIButton(
+        self.lvl_cursor_upgrade_button = UIButton(
             Rect(0,0,WIDTH*.15,HEIGHT*.1),
             ux={
                 
-                'text': f'Strength\n{int(self.cc.lvl_click * 11.1)}',
+                'text': f'Cursor: {self.cc.get_price("cursor")}',
                 'size': (WIDTH*.15,HEIGHT*.1),
                 'font':FONT(size=20),
                 'border_radius':45,
@@ -60,7 +110,58 @@ class App(Application):
                 'hovered_color': Color('#CC6600'),
                 'pressed_color': Color('#CC6600')
                 },
-            on_press_callback = self.on_upgrade_click
+            on_press_callback = self.cc.buy_cursor
+        )
+        self.lvl_grandma_upgrade_button = UIButton(
+            Rect(0,HEIGHT*.1,WIDTH*.15,HEIGHT*.1),
+            ux={
+                
+                'text': f'Grandma: {self.cc.get_price("grandma")}',
+                'size': (WIDTH*.15,HEIGHT*.1),
+                'font':FONT(size=20),
+                'border_radius':45,
+                'normal_text_color': Color('#FFE5CC'),
+                'hovered_text_color': Color('#FFF5FF'),
+                'pressed_text_color': Color('#D2BFAC'),
+                'normal_color': Color('#CC6600'),
+                'hovered_color': Color('#CC6600'),
+                'pressed_color': Color('#CC6600')
+                },
+            on_press_callback = self.cc.buy_grandma
+        )
+        self.lvl_farm_upgrade_button = UIButton(
+            Rect(0,HEIGHT*.2,WIDTH*.15,HEIGHT*.1),
+            ux={
+                
+                'text': f'Farm: {self.cc.get_price("farm")}',
+                'size': (WIDTH*.15,HEIGHT*.1),
+                'font':FONT(size=20),
+                'border_radius':45,
+                'normal_text_color': Color('#FFE5CC'),
+                'hovered_text_color': Color('#FFF5FF'),
+                'pressed_text_color': Color('#D2BFAC'),
+                'normal_color': Color('#CC6600'),
+                'hovered_color': Color('#CC6600'),
+                'pressed_color': Color('#CC6600')
+                },
+            on_press_callback = self.cc.buy_farm
+        )
+        self.lvl_mine_upgrade_button = UIButton(
+            Rect(0,HEIGHT*.3,WIDTH*.15,HEIGHT*.1),
+            ux={
+                
+                'text': f'Mine: {self.cc.get_price("mine")}',
+                'size': (WIDTH*.15,HEIGHT*.1),
+                'font':FONT(size=20),
+                'border_radius':45,
+                'normal_text_color': Color('#FFE5CC'),
+                'hovered_text_color': Color('#FFF5FF'),
+                'pressed_text_color': Color('#D2BFAC'),
+                'normal_color': Color('#CC6600'),
+                'hovered_color': Color('#CC6600'),
+                'pressed_color': Color('#CC6600')
+                },
+            on_press_callback = self.cc.buy_mine
         )
         self.cookie_label = UILabel(
             Rect(QUARTER_WIDTH*1.5,0,QUARTER_WIDTH,HEIGHT*.1),
@@ -113,34 +214,36 @@ class App(Application):
                                    ],
                                    (HALF_WIDTH,HALF_HEIGHT),time_multiplier=50)
     def update_cookie_label(self):
-        self.cookie_label.UX.text = str(self.cc.cookies)
+        self.cookie_label.UX.text = f"{self.cc.get()}" + (f"+{self.cc.get_auto_clicker():.1f}" if self.cc.get_auto_clicker() else '')
         self.cookie_label.set_image(self.cookie_label.UX.gen())
-    def on_upgrade_click(self,*_):
-        val = int(self.cc.lvl_click * 11.1)
-        nex = int((self.cc.lvl_click+1) * 11.1)
-        if self.cc.cookies >= val:
-            self.lvl_click_upgrade_button.UX.text = f"Strength\n{nex}"
-            self.lvl_click_upgrade_button.UX.draw()
-            
-            self.cc.cookies -= val
-            self.cc.lvl_click += 1
-            self.update_cookie_label()
             
     def on_cookie_click(self,*_):
         music.load("data\\bin\\click.mp3")
         music.play()
         self.animation.start_animation()
         self.cc.click()
-        self.update_cookie_label()
     def run(self):
         
         while self.is_running:
             GLOBAL_DELTA_TIME.before()
             self.window.surface.fill(Color('#FFB266'))
-            
+            #! Button image dont change !
+            if self.lvl_cursor_upgrade_button.this_frame_hovered:
+                self.lvl_cursor_upgrade_button.UX.text = f'Cursor: {self.cc.get_price("cursor")}'
+                self.lvl_cursor_upgrade_button.UX.draw()
+            if self.lvl_grandma_upgrade_button.this_frame_hovered:
+                self.lvl_grandma_upgrade_button.UX.text = f'Grandma: {self.cc.get_price("grandma")}'
+                self.lvl_grandma_upgrade_button.UX.draw()
+            if self.lvl_farm_upgrade_button.this_frame_hovered:
+                self.lvl_farm_upgrade_button.UX.text = f'Farm: {self.cc.get_price("farm")}'
+                self.lvl_farm_upgrade_button.UX.draw()
+            if self.lvl_mine_upgrade_button.this_frame_hovered:
+                self.lvl_mine_upgrade_button.UX.text = f'Mine: {self.cc.get_price("mine")}'
+                self.lvl_mine_upgrade_button.UX.draw()
             UIM.render_queue(self)
             self.animation.update()
-
+            self.cc.update()
+            self.update_cookie_label()
             self.update()
             GLOBAL_DELTA_TIME.after()
 
