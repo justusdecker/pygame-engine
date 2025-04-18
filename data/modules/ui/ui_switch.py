e1917b346e1e04350a46e5c8a3b952df10677e98
Justus <justus.d2025@gmail.com>
2025-04-18 20:12:22 +0200

    0.1.5 - fixed #84, #71 & #72

--- data/modules/ui/ui_switch.py
+++ data/modules/ui/ui_switch.py
@@ -1,12 +1,13 @@
 from data.modules.ui.ui_element import UIElement, UIC
+from data.modules.ui.ux_element import UXElement
 from data.modules.constants import MEDIUM_BACKGROUND_COLOR,TEXT_COLOR,HIGHLIGHT_TEXT_COLOR,PRESSED_TEXT_COLOR
 from pygame.draw import rect as rect_draw
-from pygame import Surface, Rect, Color, SRCALPHA
+from pygame import Surface, Rect, SRCALPHA
 
-from data.modules.ui.ux_element import UXElement
 
 
 class UXSwitch(UXElement):
+    
     def __init__(self,
                  **options) -> None:
         if not 'bcg' in options: options['bcg'] = (MEDIUM_BACKGROUND_COLOR,)
@@ -14,6 +15,7 @@ class UXSwitch(UXElement):
         super().__init__(**options)
         self.normal_off_image,self.hovered_off_image, self.pressed_off_image,self.normal_on_image,self.hovered_on_image, self.pressed_on_image = self.draw()
     def draw(self):
+        "Use this in case you want to redraw the switch."
         return self.gen([
             (self.get_color(self.text_color_group,0),False),
             (self.get_color(self.text_color_group,1),False),
@@ -23,6 +25,9 @@ class UXSwitch(UXElement):
             (self.get_color(self.text_color_group,2),True),
         ])
     def gen(self,array:list=[]):
+        """
+        Get the switch images. Off / On states.
+        """
         _ret = []
         for foreground_color,state in array:
             SURF = Surface(self.size,SRCALPHA)
@@ -36,32 +41,27 @@ class UXSwitch(UXElement):
                 rect_draw(
                     SURF,
                     foreground_color,
-                    (
-                        2,
-                        2,
-                        20,
-                        20
-                        ),
+                    (2,2,self.size[0]-4,self.size[1]-4),
                     border_radius = self.border_radius
                 )
-                
-            
-            
             _ret.append(SURF)
         return _ret
 
 class UISwitch(UIElement):
+    """
+    .. cb_on_hover:: set the callback for on_hover
+    .. cb_on_press:: set the callback for on_press
+    .. cb_on_un_hover:: set the callback for on_un_hover
+    """
     def __init__(self, rect: Rect, **kwargs):
         super().__init__(rect,**kwargs)
         UIC.add_element('uiSwitch')
         self.toggle = kwargs.get('initialValue',False)
         self.UX = UXSwitch(**kwargs.get('ux', {}))
         self.set_image(self.UX.normal_off_image)
-        self.oHC = kwargs.get('onHoveredCallback',self.noCallback)
-        
-        self.oPC = kwargs.get('onPressCallback',self.noCallback)
-        
-        self.oUHC = kwargs.get('onUnHoverCallback',self.noCallback)
+        self.oHC = kwargs.get('cb_on_hover',self.noCallback)
+        self.oPC = kwargs.get('cb_on_press',self.noCallback)
+        self.oUHC = kwargs.get('cb_on_un_hover',self.noCallback)
     def noCallback(self,_):
         pass
     def update(self):
