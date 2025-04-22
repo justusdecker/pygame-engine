f6ce7e09de0f95012257280c5e46c04b78243de4
Justus <justus.d2025@gmail.com>
2025-04-22 05:24:01 +0200

    0.1.19

--- README.md
+++ README.md
@@ -13,11 +13,13 @@ The new datamanagement system will be:
     
 
 Already implemented:
+- simple box collisions between two entitys
+- vectors
 - logging
 - Animationsystem
 - JSON Datamanagement
 - UI Elements like buttons, labels, text_inputs...
-- Some UI Elements may not work currently. I fix this this until release 0.2 (April, 30, 2025)
+- Some UI Elements may not work currently.
 
 The Cookie Clicker Clone Project:
 <img src="https://justusdeckerde.wordpress.com/wp-content/uploads/2025/04/cookie_clicker_clone_project-1.png">
@@ -27,9 +29,4 @@ Coming soon:
 - A DOOM Clone to get into pseudo 3D(variant)
 - A Mario Kart Clone to to get into pseudo 3D(variant)
 - A 2D mini RPG to get all of the stuff together & check for missing features,(text_boxes, type_writer_effect and so on!)
-- A coop game
-- A multiplayer game to get into the steamworks api
-- An animation creator for this engine
-- A pixelart texture editor
-- A GUI editor
 - for more detail: view repo project!
--- pong_debug.py
+++ pong_debug.py
@@ -5,11 +5,9 @@ from data.modules.ui.ui_element import UIM
 from data.modules.ui.ui_label import UILabel
 from data.modules.entity import Entity
 from data.modules.vector import Vector2,Vector4
-from pygame import Surface
-LOG.tobash = False
+from pygame import Surface,K_w,K_s,K_UP,K_DOWN
 from pygame.key import get_pressed
-from pygame import K_w,K_s,K_UP,K_DOWN
-
+LOG.tobash = False
 class App(Application):
     def __init__(self): 
         super().__init__()
@@ -24,15 +22,14 @@ class App(Application):
     def reset(self):
         self.move = Vector2(10,10)
         self.ball.vector = Vector4(HALF_WIDTH-(WIDTH*.025),HALF_HEIGHT-(WIDTH*.025),WIDTH*.025,WIDTH*.025)
+    def set_move(self,v:Vector2):
+        self.move *= v
+        return self.move
     def x_change(self,st:bool):
-        if st:
-            self.move *= Vector2(-1,1)
-            self.ball.vector += self.move
+        if st: self.ball.vector += self.set_move(Vector2(-1,1))
     def y_change(self,st:bool):
-        if st:
-            self.move *= Vector2(1,-1)   
-            self.ball.vector += self.move
-    def key_check(self):
+        if st: self.ball.vector += self.set_move(Vector2(1,-1))
+    def key_check(self):#! refactor this
         KEYS = get_pressed()
         if KEYS[K_w]: self.bar_left.vector += Vector2(0,-5)
         elif KEYS[K_s]: self.bar_left.vector += Vector2(0,5)
@@ -43,30 +40,20 @@ class App(Application):
             GLOBAL_DELTA_TIME.before()
             self.CLK.tick(60)
             self.window.surface.fill((0,0,0))
-            
             self.key_check()
-
             self.bar_left.collision_mbts()
             self.bar_right.collision_mbts()
-            self.bar_left.render()
-            self.bar_right.render()
-            self.ball.render()
-            
+            self.bar_left.render()#! Will be moved to the Entity Render
+            self.bar_right.render()#! Will be moved to the Entity Render
+            self.ball.render()#! Will be moved to the Entity Render
             self.x_change(self.bar_left.check_line_collision(self.ball,1))
             self.x_change(self.bar_right.check_line_collision(self.ball,0))
             self.x_change(self.ball.vector.x < 0 or self.ball.vector.x + self.ball.vector.z > WIDTH)
             self.y_change(self.ball.vector.y < 0 or self.ball.vector.y + self.ball.vector.w > HEIGHT)
-
             self.ball.vector += self.move
-
-            if self.ball.vector.x < 20:
-                self.reset()
-            if self.ball.vector.x + self.ball.vector.z > WIDTH - 20:
-                self.reset()
+            if self.ball.vector.x < 20: self.reset()
+            if self.ball.vector.x + self.ball.vector.z > WIDTH - 20: self.reset()
             self.update()
-            
             GLOBAL_DELTA_TIME.after()
-
 if __name__ == "__main__":
-    APP = App()
-    APP.run()
+    App().run()
