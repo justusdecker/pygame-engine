95b3c1e8a65d607cd0fe0ef72129cf85d0bd8f91
Justus <justus.d2025@gmail.com>
2025-04-22 02:34:00 +0200

    0.1.16

--- data/modules/ui/html_to_dict.py
+++ data/modules/ui/html_to_dict.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from json import dumps
 class HTMLWRAPPER:
     def __init__(self,html_text:str):
         self.html_text = html_text
@@ -12,17 +13,21 @@ class HTMLWRAPPER:
         for ends in ['</button>']:
             HTML = HTML.replace(ends,'')
         HTML = '\n'.join([x for x in HTML.splitlines() if x and not x.isspace()])
-        for line in HTML.splitlines():
-            
-            #get child if existing
-            
-            for x in line:
-                if not x.isspace(): break
-                depth += 1
-            
-        return HTML
+        sp_l = HTML.splitlines()
+        for idx,line in enumerate(sp_l):
+            print(line)
+            truncated = (''.join([i for i in line if not i.isspace()])).startswith('<')
+            if truncated:
+                args = line.replace('<','').replace('>','').split(' ')[2:]
+                UIE = {arg.split('=')[0]:arg.split('=')[1].replace('\"','') for arg in args}
+                
+                UIE['child'] = idx+1 if idx + 1 <= len(sp_l) else None
+                ui_elements.append(UIE)
+            else:
+                UIE['text'] = idx+1 if idx + 1 <= len(sp_l) else None
+        return ui_elements
 html = '<button element_name="test" background_colors="[]" text_colors="[]" x="width" y="height"><button element_name="test" background_colors="[]" text_colors="[]" x="width" y="height">test</button></button>'
 
 
 
-print(HTMLWRAPPER(html).unpack_html())
+print(dumps(HTMLWRAPPER(html).unpack_html(),indent=4))
