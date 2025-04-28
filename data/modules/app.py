60e505daded25eb20b5b40ac58fc2c9e3ddc7127
Justus <justus.d2025@gmail.com>
2025-04-28 14:49:14 +0200

    0.2.18 - fixed #78

--- data/modules/app.py
+++ data/modules/app.py
@@ -4,18 +4,25 @@ from pygame.event import get as get_events
 from pygame import QUIT,quit as pg_quit,Clock
 
 class Application:
+    """
+    .. window:: A pygame-engine window
+    .. is_running:: IF False - the app closes
+    .. CLK:: A pygame.Clock object
+    """
     def __init__(self):
         self.window = Window()
         self.is_running = True
         self.CLK = Clock()
 
     def update(self):
+        "Updates the window & calls the event handler"
 
         self.window.update()
         
         self.event_handler()
             
     def event_handler(self):
+        "A default pygame event handler"
         for event in get_events():
             if event.type == QUIT:
                 pg_quit()
