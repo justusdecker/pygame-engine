b553c35a7f52744905630c81347ff866ef9336ba
Justus <justus.d2025@gmail.com>
2025-05-01 09:45:07 +0200

    0.2.26 - fixed #58

--- data/modules/ui/ui_drop_down.py
+++ data/modules/ui/ui_drop_down.py
@@ -1,9 +1,25 @@
 from data.modules.ui.ui_element import UIElement,UIC
 from pygame import Surface, Rect
 from data.modules.ui.ui_button import UIButton
+from data.modules.vector import Vector4
 class UIDropDown(UIElement):
-    def __init__(self, rect: Rect, **kwargs) -> None:
-        super().__init__(rect, **kwargs)
+    """
+    .. child_instances:: 
+    
+        list[tuple[str,callback]] Child Buttons
+    .. mother_instance_button:: 
+    
+        The Main Button
+
+    UX OPTIONS
+    -----
+    .. size:: the size of all buttons in this element
+    .. text:: The text of the ``mother_instance_button``
+    .. tcg:: ``#484848`` ``#a6a6a6`` ``#ffffff``
+    .. bcg:: ``#777777``
+    """
+    def __init__(self, vector:Vector4, **kwargs) -> None:
+        super().__init__(vector, **kwargs)
         self.set_image(Surface((1,1)))
         UIC.add_element('uiDropDown')
         self.toggle = False
@@ -12,17 +28,17 @@ class UIDropDown(UIElement):
             ux['tcg'] = ux.get('tcg',('#484848','#a6a6a6','#ffffff'))
         if not 'bcg' in ux:
             ux['bcg'] = ux.get('bcg',('#777777',))
-        self.mother_instance_button = UIButton(rect,ux=ux,cb_on_press=self.switch,layer=self.layer,parent=self.parent,group=self.group)
+        self.mother_instance_button = UIButton(vector,ux=ux,cb_on_press=self.switch,layer=self.layer,parent=self.parent,group=self.group)
         self.child_instances = []
         
-        rect.x = 0
-        rect.y = 0
+        vector.x = 0
+        vector.y = 0
         self._callbacks = []
         for text,callback in kwargs.get('childs_instances',[]):
             
             ux['text'] = text
-            rect.y += 24
-            UIB = UIButton(rect,ux=ux,cb_on_press=self.btnPress,parent=self,layer=self.layer,group=self.group)
+            vector.y += 24
+            UIB = UIButton(vector,ux=ux,cb_on_press=self.btnPress,parent=self,layer=self.layer,group=self.group)
             self.child_instances.append(UIB)
             self._callbacks.append((callback,UIB.element_id))
         for e in self.child_instances:
