ad41b15a370afe533e1cbb41ec233bc174fdc2d6
Justus <justus.d2025@gmail.com>
2025-05-13 15:46:21 +0200

    0.2.56 - removed dfs function & dfs call in flood_fill

--- data/modules/algorithms.py
+++ data/modules/algorithms.py
@@ -1,18 +1,5 @@
 from pygame import Surface
-from sys import setrecursionlimit
 from queue import Queue
-def dfs(surf: Surface,i,j,old_color,new_color):
-    "!RECURSIVE FUNCTION!"
-    n, m = surf.get_size()
-    if i < 0 or i >= n or j < 0 or j >= m or surf.get_at((i,j)) != old_color:
-        #?Pixel is out of range or already recolored
-        return
-    else:
-        surf.set_at((i,j),new_color)
-        dfs(surf,i+1,j,old_color,new_color)
-        dfs(surf,i-1,j,old_color,new_color)
-        dfs(surf,i,j+1,old_color,new_color)
-        dfs(surf,i,j-1,old_color,new_color)
 def flood_fill(surf: Surface,i,j,new_color):
     n, m = surf.get_size()
     old_color = surf.get_at((i,j))
@@ -30,5 +17,3 @@ def flood_fill(surf: Surface,i,j,new_color):
             queue.put((i-1,j))
             queue.put((i,j+1))
             queue.put((i,j-1))
-    dfs(surf,i,j,old_color,new_color)
-    setrecursionlimit(1000)
