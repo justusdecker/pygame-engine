0a1e81088c22f63203217c8de0163360e2360cba
Justus <justus.d2025@gmail.com>
2025-05-17 22:50:07 +0200

    0.3.11

--- data/modules/platformer_rendering/tilemap.py
+++ data/modules/platformer_rendering/tilemap.py
@@ -4,7 +4,7 @@ from pygame import Surface
 from random import randint
 class TileMap:
     def __init__(self,app,surf: Surface,tile_size: Vector2,scaled_size: Vector2,t_map:list[list[int]]):
-        self.t_map = [[randint(0,4) for i in range(64)] for x in range(36)]
+        self.t_map = t_map
 
         self.pos = Vector2(0,0)
         self.tileset = TileSet(app,surf,tile_size,scaled_size)
--- main_2D_platformer.py
+++ main_2D_platformer.py
@@ -13,15 +13,16 @@ class App(Application):
     def __init__(self):
         super().__init__()
         #self.tileset = TileSet(self,load('data\\bin\\img\\test_tileset.png'),Vector2(16,16),Vector2(32,32))
-        self.tilemap = TileMap(self,load('data\\bin\\img\\test_tileset.png'),Vector2(16,16),Vector2(20,20),None)
+        arr = [[2 for x in range(32)] for i in range(17)]
+        arr.append([0 for x in range(32)])
+        
+        self.tilemap = TileMap(self,load('data\\bin\\img\\test_tileset.png'),Vector2(16,16),Vector2(40,40),arr)
     def run(self):
 
         while self.is_running:
             GLOBAL_DELTA_TIME.before()
             self.window.surface.fill((36,36,36))
             self.tilemap.render()
-            #self.tileset.render(Vector2(132,132),4)
-            #self.tileset.render(Vector2(164,164),5)
             
             self.update()
             GLOBAL_DELTA_TIME.after()
