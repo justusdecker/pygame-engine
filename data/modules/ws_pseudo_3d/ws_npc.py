05cb7d77c64c79ac3c1d72d50b94f4d3d748c7e6
Justus <justus.d2025@gmail.com>
2025-05-19 14:24:23 +0200

    0.3.33 - start on npcs

---
+++ data/modules/ws_pseudo_3d/ws_npc.py
@@ -0,0 +1,24 @@
+from data.modules.ws_pseudo_3d.ws_sprite_object import *
+from random import randint, random, choice
+"""
+Folderstructure:
+
+data\\
+    img\\
+        {object}\\
+            attack\\
+                ...
+            death\\
+                ...
+            idle\\
+                ...
+            pain\\
+                ...
+            walk\\
+                ...
+            0.png
+            
+"""
+class NPC:
+    def __init__(self):
+        pass
