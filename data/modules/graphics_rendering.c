58e6b280d36382ed41eececd0fd95ede7d1c9807
Justus <justus.d2025@gmail.com>
2025-05-22 17:19:14 +0200

    0.3.49

---
+++ data/modules/graphics_rendering.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <math.h>
+
+char *ConvertHsvToRgb(float H, float S, float V) {
+    float r, g, b;
+    
+
+    // First, we get the hue (H), saturation (S), brightness (V ), where H is in a scale
+    // between 0 to 6 inclusively, and S and V in a scale between 0 to 1.
+
+    float h = H / 360;
+    float s = S / 100;
+    float v = V / 100;
+
+    //Incidentally, the brightness, V , also happens to represent the brightest channel in our resulting RGB colour.
+
+    int i = floor(h * 6);
+    float f = h * 6 - i;
+
+	float x = v * (1 - s);
+	float y = v * (1 - f * s);
+	float z = v * (1 - (1 - f) * s);
+
+    // V(v) alpha(x) beta(y) gamma(z)
+    switch (i % 6) {
+		case 0: r = v, g = z, b = x; break;
+		case 1: r = y, g = v, b = x; break;
+		case 2: r = x, g = v, b = z; break;
+		case 3: r = x, g = y, b = v; break;
+		case 4: r = z, g = x, b = v; break;
+		case 5: r = v, g = x, b = y; break;
+	}
+    static char rgb[3];
+
+    rgb[0] = (char)r * 255;
+    rgb[1] = (char)g * 255;
+    rgb[2] = (char)b * 255;
+    return rgb;
+    
+}
+
+
+char *ColorRect(float hue) {
+    unsigned char w = 0;
+    unsigned char z = 0;
+    for (unsigned x = 0; x < 255; x++) {
+        for (unsigned y = 0; y < 255; y++) {
+            w++;
+            }
+        z++;
+    }
+    printf("%d %d",w,z);
+}
--- use_c.py
+++ use_c.py
@@ -1,21 +1,8 @@
 #gcc -fPIC -shared -o
 import ctypes
 from time import perf_counter
-clib = ctypes.CDLL('data\\c_lib.so')
+clib = ctypes.CDLL('data\\modules\\graphics_rendering.so')
 def conv(a: int):
     return a ** 2
 
-
-def run_10M():
-    t = perf_counter()
-    for i in range(19_000_000):
-        conv(i)
-    dt = perf_counter() - t
-    print(f"finished python code in {dt}")
-    return dt
-t = perf_counter()
-clib.run_10m()
-dt_a = perf_counter() - t
-print(f"finished c code in {dt_a}")
-dt_b = run_10M()
-print(f"The calculated delta time between python & C is {(dt_b / dt_a) * 100:.2f}%")
+clib.ColorRect(ctypes.c_float(0.2))
