26da502dcbbef392832cef505420e5c010aeaeb8
Justus <justus.d2025@gmail.com>
2025-05-23 20:34:09 +0200

    0.3.60 - fixed #233

--- data/modules/pixelarray_manipulation.c
+++ data/modules/pixelarray_manipulation.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 unsigned char *ColorTest(float scale, unsigned char* pxarr,int size) {
     
-    unsigned char *pixel_array = malloc(sizeof(pxarr));
-    printf("%d",size);
+    unsigned char *pixel_array = malloc(size);
     for (unsigned int x = 0; x <= 255; x++) {
 
         for (unsigned int y = 0; y <= 15; y++) {
@@ -12,7 +11,7 @@ unsigned char *ColorTest(float scale, unsigned char* pxarr,int size) {
                 
                 //this line here is faulty VVV
                 //printf("%d",(unsigned char)(pxarr[(x*16+y) * 3] * scale));
-                //pixel_array[(x*16+y) * 3] = 
+                pixel_array[(x*16+y) * 3 + z] = (unsigned char)(255);
                 
             }
             
