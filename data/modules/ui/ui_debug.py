a0d9957b0215a992c78dcbf3814fc6138fb9ca7f
Justus <justus.d2025@gmail.com>
2025-05-25 12:53:16 +0200

    0.3.63 - fixed #167

--- data/modules/ui/ui_debug.py
+++ data/modules/ui/ui_debug.py
@@ -1,6 +1,10 @@
 from pygame import Rect,Surface
 from pygame.draw import rect as draw_rect, line as draw_line
-
+from data.modules.ui.ui_element import UIElement, UIC
+from data.modules.ui.ui_label import UILabel
+from data.modules.ui.ui_font import FONT
+from data.modules.vector import Vector4
+from data.modules.constants import GLOBAL_DELTA_TIME
 def outliner(surf: Surface, rect: Rect,o_type:str):
     """
     Use this to make the outlines of UI / UX Elements visible!
@@ -19,4 +23,30 @@ def outliner(surf: Surface, rect: Rect,o_type:str):
         draw_line(surf,(192,0,0) if o_type else (0,192,0),(rect.w//2,i),(rect.w//2,i + 5),width=1)
     for i in range(0,rect.w,10):
         draw_line(surf,(192,0,0) if o_type else (0,192,0),(i,rect.h//2),(i + 5,rect.h//2),width=1)
-    
+
+class UIDebug(UIElement):
+    def __init__(self, vector, **kwargs):
+        super().__init__(vector, **kwargs)
+        self.fps_lable = UILabel(
+            Vector4(0,0,128,48),
+            ux={'text': "","size": (128,48),'font': FONT(size=30),'tcg': ('#ffffff',),'bcg': ('#77777744',)},layer=self.layer,parent=self
+        )
+        self.rt_lable = UILabel(
+            Vector4(128,0,128,48),
+            ux={'text': "","size": (128,48),'font': FONT(size=30),'tcg': ('#ffffff',),'bcg': ('#77777744',)},layer=self.layer,parent=self
+        )
+        self.rt_lables = []
+        self.timings = []
+        if 'render_times' in kwargs:
+            for i in range(kwargs['render_times']):
+                uil = UILabel(Vector4(0,(i+1)*48,256,48),ux={'text': "","size": (256,48),'font': FONT(size=30),'tcg': ('#ffffff',),'bcg': ('#77777744',)},layer=self.layer,parent=self)
+                self.rt_lables.append(uil)
+    def add_timings(self,t:list):
+        self.timings = t
+    def update(self):
+        _rt = GLOBAL_DELTA_TIME.get()
+        for rt,t in zip(self.rt_lables, self.timings):
+            rt.render(f'{t:.2f}ms')
+        self.fps_lable.render(f'{int(1//_rt)}fps' if _rt else 'undefined')
+        self.rt_lable.render(f'{_rt*1000:.1f}ms' if _rt else 'undefined')
+        return super().update()
--- main_pseudo_3d.py
+++ main_pseudo_3d.py
@@ -1,7 +1,7 @@
 from data.modules.constants import *
 from data.modules.app import Application
 
-from data.modules.vector import Vector2
+from data.modules.vector import Vector4
 from data.modules.ws_pseudo_3d.ws_map import Map
 from data.modules.ws_pseudo_3d.ws_player import Player
 from data.modules.ws_pseudo_3d.ws_ray_casting import RayCasting
@@ -11,9 +11,12 @@ from data.modules.ws_pseudo_3d.ws_object_handler import ObjectHandler
 from data.modules.ws_pseudo_3d.ws_weapon_handler import WeaponHandler
 from data.modules.algorithms import PathFinding
 from data.modules.audio_handler import AudioHandler
+from data.modules.ui.ui_element import UIM
+from data.modules.ui.ui_debug import UIDebug
 from pygame.mouse import set_visible
 from pygame.image import load
 import pygame.key as keys
+from time import perf_counter
 def test_print(*args):
     print(args, "Hello World!")
 
@@ -25,6 +28,7 @@ class App(Application):
         arr = [[2 for x in range(32)] for i in range(17)]
         arr.append([0 for x in range(32)])
         set_visible(False)
+        self.deb = UIDebug(Vector4(0,0,1,1),render_times=6)
         self.new_game()
     def new_game(self):
         self.map = Map(self)
@@ -44,12 +48,26 @@ class App(Application):
             GLOBAL_DELTA_TIME.before()
             #self.window.surface.fill((36,36,36))
             #self.map.draw()
+            tarray = []
+            t = perf_counter()
             self.player.update()
+            tarray.append((perf_counter() - t) * 1000)
+            t = perf_counter()
             self.raycasting.update()
+            tarray.append((perf_counter() - t) * 1000)
+            t = perf_counter()
             self.obj_handler.update()
-            
+            tarray.append((perf_counter() - t) * 1000)
+            t = perf_counter()
             self.object_renderer.draw()
+            tarray.append((perf_counter() - t) * 1000)
+            t = perf_counter()
             self.weapon.update()
+            tarray.append((perf_counter() - t) * 1000)
+            t = perf_counter()
+            UIM.render_queue(self)
+            tarray.append((perf_counter() - t) * 1000)
+            self.deb.add_timings(tarray)
             self.update()
             GLOBAL_DELTA_TIME.after()
 if __name__ == "__main__":
